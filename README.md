bce17699a116a637fee09dc012d13ae95679deff
tmarques2 <thainaramarques258@gmail.com>
2025-09-24 11:19:19 -0300

    sla

--- README.md
+++
@@ -1,1 +0,0 @@
-# API Jovens Titãs em Ação
--- models.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-class PersonagensJovensTitas(BaseModel):
-    id: Optional[int] = None
-    nome: str
-    idade: int
-    habilidade: str
-    foto: Optional[str] = None
